ac469fe1855916e6f2a5564f362be1f9b7d69c58
pnvnd <peter@datacrunch.ca>
2021-12-16 13:06:42 -0500

    Cleaned up files for demo branch

--- datacrunch-consulting/webserver.py
+++ datacrunch-consulting/webserver.py
@@ -25,14 +25,6 @@ def index():
 def ping():
     return jsonify(ping="pong")
 
-@flaskapp.route("/about")
-def about():
-    return render_template("about.html", title="Datacrunch - About")
-
-@flaskapp.route("/projects")
-def projects():
-    return render_template("projects.html", title="Datacrunch - Projects")
-
 # API to convert Fahrenheit to Celcius
 @flaskapp.route("/projects/convertC/<tempF>")
 def convertC(tempF):
@@ -46,20 +38,6 @@ def convertF(tempC):
     tempF = 9/5*(float(tempC))+32
     logger.info(f"[INFO] Converted {tempC}°C to {tempF:.2f}°F")
     return f"{tempC}°C is {tempF:.2f}°F."
-
-### Add Applications Here ###
-
-# API to calculate the nth prime number and how long it takes
-from projects.prime import prime
-flaskapp.register_blueprint(prime)
-
-# API to calculate the nth fibonacci number
-from projects.fibonacci import fibonacci
-flaskapp.register_blueprint(fibonacci)
-
-# Get COVID data and plot on chart
-from projects.covid import covid
-flaskapp.register_blueprint(covid)
 
 # Run Flask Web Application
 if __name__ == "__main__":
