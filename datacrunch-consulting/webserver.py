d1737798d71ac47762e35145f7d31fb96fcc61ca
pnvnd <peter@datacrunch.ca>
2021-12-16 13:12:33 -0500

    Cleaned up code to simplify

--- datacrunch-consulting/webserver.py
+++ datacrunch-consulting/webserver.py
@@ -1,11 +1,10 @@
-# .\venv\Scripts\activate.ps1
-# python .\datacrunch-consulting\webserver.py
+import newrelic.agent
+newrelic.agent.initialize()
 
-# import newrelic.agent
-# newrelic.agent.initialize()
 import logging
 from flask import Flask, render_template, jsonify
 
+# Set up logging
 logger = logging.getLogger("Basic Logger")
 logger.setLevel(logging.INFO)
 
@@ -19,21 +18,21 @@ flaskapp = Flask(__name__, static_url_path="/")
 # Navigation
 @flaskapp.route("/")
 def index():
-    return render_template("index.html", title="Flask Web Application")
+    return render_template("index.html", title="Flask Demo Application")
 
 @flaskapp.route("/ping/", strict_slashes=False)
 def ping():
     return jsonify(ping="pong")
 
 # API to convert Fahrenheit to Celcius
-@flaskapp.route("/projects/convertC/<tempF>")
+@flaskapp.route("/convertC/<tempF>")
 def convertC(tempF):
     tempC = (5/9*(float(tempF))-32)
     logger.info(f"[INFO] Converted {tempF}°F to {tempC:.2f}°C.")
     return f"{tempF}°F is {tempC:.2f}°C."
 
 # API to convert Celcius to Fahrenheit
-@flaskapp.route("/projects/convertF/<tempC>")
+@flaskapp.route("/convertF/<tempC>")
 def convertF(tempC):
     tempF = 9/5*(float(tempC))+32
     logger.info(f"[INFO] Converted {tempC}°C to {tempF:.2f}°F")
