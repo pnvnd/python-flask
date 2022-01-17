c376424f58859bd19d29711dcd6420c2dbd90785
pnvnd <peter@datacrunch.ca>
2022-01-17 16:25:34 -0500

    testing another redis fix on Heroku

--- datacrunch-consulting/projects/redispy.py
+++ datacrunch-consulting/projects/redispy.py
@@ -6,11 +6,10 @@ import redis
 # Flask Blueprint Application
 redispy = Blueprint("redispy", "redispy")
 
-url = urlparse(os.environ.get("REDIS_URL"))
-r = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
-# r = redis.Redis(host='localhost', port=6379, db=0)
-
 @redispy.route("/projects/redis", strict_slashes=False)
 def index():
+    url = urlparse(os.environ.get("REDIS_URL"))
+    r = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
+    # r = redis.Redis(host='localhost', port=6379, db=0)
     r.set('foo', 'bar')
     return r.get('foo')
