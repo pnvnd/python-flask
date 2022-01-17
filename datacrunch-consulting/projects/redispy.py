4e84a588f29e1e6b552e981f9841e6bd7e18de02
pnvnd <peter@datacrunch.ca>
2022-01-17 16:15:03 -0500

    Changed heroku redis to require TLS

--- datacrunch-consulting/projects/redispy.py
+++ datacrunch-consulting/projects/redispy.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, render_template
 import os
+from urllib.parse import urlparse
 import redis
 
 # Flask Blueprint Application
 redispy = Blueprint("redispy", "redispy")
 
+url = urlparse(os.environ.get("REDIS_URL"))
+r = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
+# r = redis.Redis(host='localhost', port=6379, db=0)
+
 @redispy.route("/projects/redis", strict_slashes=False)
 def index():
-    r = redis.from_url(os.environ.get("REDIS_URL"))
     r.set('foo', 'bar')
     return r.get('foo')
