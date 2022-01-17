f70a5e4dd2abd6a69f9664483ea4ec88aac5cb86
pnvnd <peter@datacrunch.ca>
2022-01-17 16:32:02 -0500

    Revert changed to redis

--- datacrunch-consulting/projects/redispy.py
+++ datacrunch-consulting/projects/redispy.py
@@ -1,15 +1,22 @@
 from flask import Blueprint, render_template
 import os
-from urllib.parse import urlparse
 import redis
+# from urllib.parse import urlparse
 
 # Flask Blueprint Application
 redispy = Blueprint("redispy", "redispy")
 
 @redispy.route("/projects/redis", strict_slashes=False)
 def index():
-    url = urlparse(os.environ.get("REDIS_URL"))
-    r = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
+    ## Heroku
+    r = redis.from_url(os.environ.get("REDIS_URL"))
+
+    ## localhost
     # r = redis.Redis(host='localhost', port=6379, db=0)
+
+    ## TLS Required
+    # url = urlparse(os.environ.get("REDIS_URL"))
+    # r = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
+    
     r.set('foo', 'bar')
     return r.get('foo')
